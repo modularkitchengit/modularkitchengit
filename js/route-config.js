b787a6e82af03720c42a9bdc8b3f83de64b190a5
Shahul Hameed <shahthepro@gmail.com>
2017-06-01 12:33:30 +0530

    Update content, Folder structure and switch to Angular UI Router

--- js/route-config.js
+++ js/route-config.js
@@ -3,24 +3,43 @@
     angular.module('app')
         .config(routeConfig);
 
-    routeConfig.$inject = ['$routeProvider'];
+    routeConfig.$inject = ['$stateProvider'];
 
-    function routeConfig($routeProvider) {
-        $routeProvider
-            .when('/', {
+    function routeConfig($stateProvider) {
+
+        $stateProvider
+			.state('app', {
+				abstract: true,
+				url: '',
+				template: '<ui-view/>'
+			})
+            .state('app.home', {
+                url: '',
                 templateUrl: 'views/home.html'
             })
-            .when('/about', {
+            .state('app.about', {
+                url: '/about',
                 templateUrl: 'views/about.html'
             })
-            .when('/gallery', {
-                templateUrl: 'views/gallery.html'
-            })
-            .when('/contact', {
+            .state('app.contact', {
+                url: '/contact',
                 templateUrl: 'views/contact.html'
             })
-            .otherwise({
-                redirectTo: '/'
+            .state('app.gallery', {
+                url: '/gallery',
+                templateUrl: 'views/gallery.html'
+            })
+            .state('app.specs', {
+                url: '/specs',
+                templateUrl: 'views/specs.html'
+            })
+            .state('app.designs', {
+                url: '/designs',
+                templateUrl: 'views/designs.html'
+            })
+            .state('app.products', {
+                url: '/products',
+                templateUrl: 'views/products.html'
             });
     }
 }());
