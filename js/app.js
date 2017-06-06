7a296324f19e2a1e7084c1b425b277310e823f8e
Shahul Hameed <shahthepro@gmail.com>
2017-06-06 11:23:44 +0530

    Minor fix

--- js/app.js
+++ js/app.js
@@ -3,27 +3,4 @@
         'ui.router',
         'ngSanitize'
     ]);
-
-    angular.module('app').run(function($rootScope, $window, $location) {
-        $rootScope.$on('$includeContentLoaded', function() {
-            $window.mobileMenuOutsideClick();
-            $window.offcanvasMenu();
-            $window.burgerMenu();
-            $window.contentWayPoint();
-            $window.sliderMain();
-            $window.dropdown();
-            $window.goToTop();
-            $window.loaderPage();
-            $window.counterWayPoint();
-            $window.fullHeight();
-            $window.parallax();
-            $window.DateTimePickerFunc();
-
-            $window.jQuery('.fh5co-bg-img').each(function(){
-                $window.jQuery(this).css('width', '100%');
-            });
-            $window.zoomFunc();
-        });
-    });
-
 }());
